3e79857e0036c55fcce110449d6a457f8e0b7c62
abc <abc@example.com>
2017-08-26 15:34:44 +0800

    modify pusher success

--- app/Events/TestEvent.php
+++ app/Events/TestEvent.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
-class TestEvent
+class TestEvent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
     public $msg;
